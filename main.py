b0d8e878a9e9022da67e1e5dd1a5feefb7946e82
ovo-Tim <ovo-tim@qq.com>
2024-07-27 14:43:12 +0800

    clean code

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ logging.basicConfig(level=logging.INFO)
 
 class _conf:
     def __init__(self, conf_dir:Path) -> None:
+        if not conf_dir.exists():
+            logging.info(f"Create config directory at {conf_dir}")
+            # conf_dir.mkdir(parents=True)
+            shutil.copytree(f'{dirname(__file__)}/defaults', conf_dir)
+
         with open(conf_dir/'config.json') as f:
             self.conf = ujson.load(f)
 
@@ -22,14 +27,11 @@ class _conf:
         return self.conf[key]
 
 conf_dir = Path('~/.furlang').expanduser()
-if not conf_dir.exists():
-    logging.info(f"Create config directory at {conf_dir}")
-    # conf_dir.mkdir(parents=True)
-    shutil.copytree(f'{dirname(__file__)}/defaults', conf_dir)
-
 conf = _conf(conf_dir)
 sta = statistic.statistic(conf['statistic_path'])
 datas = database.datas(conf['db_path'], conf['excluded_words_path'], sta)
 dictionary = dicts.dicts(conf['dicts_path'])
 ser = server.init(datas, dictionary, port=conf['port'])
-ser.serve_forever()
+
+if __file__ == '__main__':
+    ser.serve_forever()
